a23b42d8a09efe623dc811b6bffc0fe8c0a3c9fb
Gui Rambo <insidegui@gmail.com>
2022-06-29 19:39:41 -0300

    Moved NSAlert confirmation extension into a separate file

--- VirtualBuddy/Session/VirtualMachineSessionView.swift
+++ VirtualBuddy/Session/VirtualMachineSessionView.swift
@@ -206,31 +206,3 @@ extension VMController {
         return true
     }
 }
-
-extension NSAlert {
-
-    static func runConfirmationAlert(title: String,
-                                     message: String,
-                                     continueButtonTitle: String = "Continue",
-                                     cancelButtonTitle: String = "Cancel") async -> Bool
-    {
-        let alert = NSAlert()
-
-        alert.messageText = title
-        alert.informativeText = message
-
-        alert.addButton(withTitle: cancelButtonTitle)
-        alert.addButton(withTitle: continueButtonTitle)
-
-        let response: NSApplication.ModalResponse
-
-        if let window = NSApp?.keyWindow {
-            response = await alert.beginSheetModal(for: window)
-        } else {
-            response = alert.runModal()
-        }
-
-        return response == .alertSecondButtonReturn
-    }
-
-}
