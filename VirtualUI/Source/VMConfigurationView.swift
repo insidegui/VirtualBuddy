c0002b4810067fd1a0427c80bc4f32c2510b7bbc
Gui Rambo <insidegui@gmail.com>
2022-07-17 19:08:15 -0300

    Created VirtualUI framework to hold UI components in order to improve the ability to use SwiftUI previews

--- VirtualUI/Source/VMConfigurationView.swift
+++ VirtualUI/Source/VMConfigurationView.swift
@@ -8,9 +8,13 @@
 import SwiftUI
 import VirtualCore
 
-struct VMConfigurationView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+public struct VMConfigurationView: View {
+    @EnvironmentObject var controller: VMController
+
+    public init() { }
+
+    public var body: some View {
+        Text("Hello, World!")
     }
 }
 
@@ -18,6 +22,9 @@ struct VMConfigurationView: View {
 struct VMConfigurationView_Previews: PreviewProvider {
     static var previews: some View {
         VMConfigurationView()
+            .environmentObject(VMController(with: .preview))
+            .padding()
     }
 }
+
 #endif
