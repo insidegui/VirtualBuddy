c317a26eb0fbe5b56522ef5e731b98bb9703adbb
Gui Rambo <insidegui@gmail.com>
2022-07-20 10:20:56 -0300

    Added error to prevent release builds missing the managed entitlement

--- VirtualBuddy/Bootstrap/VirtualBuddyAppDelegate.swift
+++ VirtualBuddy/Bootstrap/VirtualBuddyAppDelegate.swift
@@ -7,6 +7,10 @@
 
 import Cocoa
 
+#if BUILDING_NON_MANAGED_RELEASE
+#error("Trying to build for release without using the managed scheme. This build won't include managed entitlements. This error is here for Rambo, you may safely comment it out and keep going.")
+#endif
+
 @objc final class VirtualBuddyAppDelegate: NSObject, NSApplicationDelegate {
     
     func applicationWillFinishLaunching(_ notification: Notification) {
