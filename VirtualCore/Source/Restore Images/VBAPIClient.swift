5bc33ea75b4c7ccd3233241c955ead530fe7bb3e
Gui Rambo <insidegui@gmail.com>
2023-03-06 13:12:17 -0300

    New restore images API with support for Linux

--- VirtualCore/Source/Restore Images/VBAPIClient.swift
+++ VirtualCore/Source/Restore Images/VBAPIClient.swift
@@ -9,14 +9,35 @@ import Foundation
 
 public final class VBAPIClient {
 
+    public enum GuestType {
+        case macOS
+        case Linux
+
+        var path: String {
+            switch self {
+            case .macOS:
+                return "/restore/mac"
+            case .Linux:
+                return "/restore/linux"
+            }
+        }
+    }
+
     public struct Environment: Hashable {
-        public var baseURL = URL(string: "https://virtualbuddy-api-dev.bestbuddyapps3496.workers.dev")!
-        public var apiKey = "15A25D48-4A34-4EE4-A293-C22B0DE1B54E"
+        public var baseURL: URL
+        public var apiKey: String
+
+        #if DEBUG
+        public static let local = Environment(
+            baseURL: URL(string: "https://virtualbuddy.ngrok.io")!,
+            apiKey: "15A25D48-4A34-4EE4-A293-C22B0DE1B54E"
+        )
 
         public static let development = Environment(
             baseURL: URL(string: "https://virtualbuddy-api-dev.bestbuddyapps3496.workers.dev")!,
             apiKey: "15A25D48-4A34-4EE4-A293-C22B0DE1B54E"
         )
+        #endif
 
         public static let production = Environment(
             baseURL: URL(string: "https://api.virtualbuddy.app")!,
@@ -24,13 +45,22 @@ public final class VBAPIClient {
         )
 
         public static var current: Environment {
-            if let override = UserDefaults.standard.string(forKey: "VBAPIEnvironment"),
-               override == "development"
-            {
-                return .development
+            #if DEBUG
+            if let override = UserDefaults.standard.string(forKey: "VBAPIEnvironment") {
+                if override == "development" {
+                    return .development
+                } else if override == "local" {
+                    return .local
+                } else {
+                    assertionFailure("Unknown API environment: \(override)")
+                    return .production
+                }
             } else {
                 return .production
             }
+            #else
+            return .production
+            #endif
         }
     }
 
@@ -54,8 +84,8 @@ public final class VBAPIClient {
     }
 
     @MainActor
-    public func fetchRestoreImages() async throws -> [VBRestoreImageInfo] {
-        let req = request(for: "restore")
+    public func fetchRestoreImages(for guest: GuestType) async throws -> [VBRestoreImageInfo] {
+        let req = request(for: guest.path)
 
         let (data, res) = try await URLSession.shared.data(for: req)
 
--- VirtualUI/Source/Installer/Steps/RestoreImagePicker.swift
+++ VirtualUI/Source/Installer/Steps/RestoreImagePicker.swift
@@ -18,10 +18,10 @@ final class RestoreImagePickerController: ObservableObject {
     @Published var errorMessage: String?
     @Published var cookie: String?
     
-    func loadRestoreImageOptions() {
+    func loadRestoreImageOptions(for guest: VBAPIClient.GuestType) {
         Task {
             do {
-                let images = try await api.fetchRestoreImages()
+                let images = try await api.fetchRestoreImages(for: guest)
                 
                 await MainActor.run {
                     self.restoreImageOptions = images
@@ -96,7 +96,7 @@ struct RestoreImagePicker: View {
             selection = $0
             validationChanged.send(controller.validateSelectedRestoreImage())
         })
-        .onAppearOnce { controller.loadRestoreImageOptions() }
+        .onAppearOnce { controller.loadRestoreImageOptions(for: .macOS) }
 
         if let selectedImage = controller.selectedRestoreImage,
            let advisory = controller.restoreAdvisory(for: selectedImage)
@@ -145,12 +145,12 @@ struct RestoreImagePicker: View {
         .padding(.top)
     }
     
-    @State private var authRequirementFlow: VBRestoreImageInfo.AuthRequirement?
+    @State private var authRequirementFlow: VBGuestReleaseChannel.Authentication?
 
     @ViewBuilder
-    private func authenticationEntryPoint(with requirement: VBRestoreImageInfo.AuthRequirement) -> some View {
+    private func authenticationEntryPoint(with requirement: VBGuestReleaseChannel.Authentication) -> some View {
         VStack(spacing: 16) {
-            Text(requirement.explainer)
+            Text(requirement.note)
                 .font(.system(size: 12))
                 .lineSpacing(1.2)
                 .foregroundColor(.secondary)
@@ -179,7 +179,7 @@ struct RestoreImagePicker: View {
         .controlSize(.large)
         .multilineTextAlignment(.center)
         .sheet(item: $authRequirementFlow, content: { requirement in
-            AuthenticatingWebView(url: requirement.signInURL) { cookies in
+            AuthenticatingWebView(url: requirement.url) { cookies in
                 guard let headerValue = requirement.satisfiedCookieHeaderValue(with: cookies) else { return }
                 self.controller.cookie = headerValue
                 self.authRequirementFlow = nil
