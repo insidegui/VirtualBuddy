830ae11c5319c572197478d8f386ea0aa83a45db
Gui Rambo <insidegui@gmail.com>
2023-03-09 16:24:01 -0300

    Updating installed VirtualBuddyGuest when mounted image has different build or higher version

--- VirtualBuddyGuest/GuestAppInstaller.swift
+++ VirtualBuddyGuest/GuestAppInstaller.swift
@@ -7,8 +7,16 @@ final class GuestAppInstaller {
 
     func installIfNeeded() throws {
         do {
+            let needsUpdate = mountedGuestImageNeedsInstall
+
+            guard !needsUpdate else {
+                logger.notice("🚀 Relaunching from mounted image for update")
+                try NSApplication.shared.relaunch(at: mountedImageAppURL.path)
+                return
+            }
+
             guard needsInstall else {
-                logger.debug("Install not needed: running from supported path \(Bundle.main.bundleURL.deletingLastPathComponent().path, privacy: .public)")
+                logger.debug("Install not needed: running from supported path and updated not needed. Path: \(Bundle.main.bundleURL.deletingLastPathComponent().path, privacy: .public)")
                 return
             }
 
@@ -57,6 +65,74 @@ final class GuestAppInstaller {
         return false
     }
 
+    private var mountedImageAppURL: URL { URL(fileURLWithPath: "/Volumes/Guest/VirtualBuddyGuest.app") }
+
+    /// `true` if there's a VirtualBuddyGuest image mounted at `/Volumes/Guest`
+    /// for which the following conditions are true:
+    /// 1 - The guest in the volume has a different `VBGuestBuildID` from this process
+    /// 2 - The guest in the volume has a `CFBundleVersion` that's **greater than or equal to** the `CFBundleVersion` of this process
+    private var mountedGuestImageNeedsInstall: Bool {
+        guard FileManager.default.fileExists(atPath: "/Volumes/Guest") else { return false }
+
+        logger.debug("Guest volume is mounted, checking app version")
+
+        let imageURL = mountedImageAppURL
+
+        guard imageURL.path != Bundle.main.bundleURL.path else {
+            logger.debug("We're the mounted image app, skipping update checks.")
+            return false
+        }
+
+        guard let imageBundle = Bundle(url: imageURL) else {
+            logger.error("Couldn't get bundle at \(imageURL.path, privacy: .public)")
+            return false
+        }
+
+        guard let imageBuildID = imageBundle.vbGuestBuildID else {
+            logger.error("Couldn't find VBGuestBuildID in image bundle")
+            return false
+        }
+
+        guard let imageBundleVersion = imageBundle.bundleVersion else {
+            logger.error("Couldn't find CFBundleVersion in image bundle")
+            return false
+        }
+
+        guard let currentBuildID = Bundle.main.vbGuestBuildID else {
+            logger.error("Couldn't find VBGuestBuildID in current bundle")
+            return false
+        }
+
+        guard let currentBundleVersion = Bundle.main.bundleVersion else {
+            logger.error("Couldn't find CFBundleVersion in current bundle")
+            return false
+        }
+
+        guard imageBuildID != currentBuildID else {
+            logger.debug("Image build ID is same as current build ID (\(currentBuildID, privacy: .public)), update won't be performed")
+            return false
+        }
+
+        guard imageBundleVersion >= currentBundleVersion else {
+            logger.debug("Image build ID differs from current build ID, but image has a lower CFBundleVersion (\(imageBundleVersion, privacy: .public)), ignoring")
+            return false
+        }
+
+        logger.notice("Mounted image qualifies for update with CFBundleVersion \(imageBundleVersion, privacy: .public), VBGuestBuildID \(imageBuildID, privacy: .public)")
+
+        return true
+    }
+
+}
+
+extension Bundle {
+    var bundleVersion: Int? {
+        guard let str = infoDictionary?[kCFBundleVersionKey as String] as? String else { return nil }
+        return Int(str)
+    }
+    var vbGuestBuildID: String? {
+        infoDictionary?["VBGuestBuildID"] as? String
+    }
 }
 
 extension NSApplication {
