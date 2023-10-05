08dafb2c9748a7d0205adc2ae94cf67c4a66e963
Felix Ritter <felix.ritter@mevis.fraunhofer.de>
2023-10-05 13:44:36 +0200

    Enable copy-and-paste support between the host and a Linux guest

--- VirtualCore/Source/Virtualization/VMInstance.swift
+++ VirtualCore/Source/Virtualization/VMInstance.swift
@@ -124,6 +124,9 @@ public final class VMInstance: NSObject, ObservableObject {
         c.entropyDevices = helper.createEntropyDevices()
         c.audioDevices = model.configuration.vzAudioDevices
         c.directorySharingDevices = try model.configuration.vzSharedFoldersFileSystemDevices
+        if model.configuration.systemType == .linux, #available(macOS 13.0, *) {
+            c.consoleDevices = [(helper as! LinuxVirtualMachineConfigurationHelper).createSpiceAgentConsoleDeviceConfiguration()]
+        }
 
         let bootDevice = try await helper.createBootBlockDevice()
         let additionalBlockDevices = try await helper.createAdditionalBlockDevices()
