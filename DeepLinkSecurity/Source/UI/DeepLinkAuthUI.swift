17839e8cb5fb74bceb21fc7651b966872c33676e
Patrick Linnane <patrick@linnane.io>
2024-01-12 12:30:21 -0800

    various: fix miscellaneous typos
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- DeepLinkSecurity/Source/UI/DeepLinkAuthUI.swift
+++ DeepLinkSecurity/Source/UI/DeepLinkAuthUI.swift
@@ -4,7 +4,7 @@ public protocol DeepLinkAuthUI: AnyObject {
     /// Return ``DeepLinkClientAuthorization/authorized`` if the user has allowed the client to open deep links in the app.
     /// 
     /// If this method throws, then the auth store is not modified and the user will be prompted again the next time the same client
-    /// attepts to open a deep link in the app.
+    /// attempts to open a deep link in the app.
     ///
     /// If this method returns ``DeepLinkClientAuthorization/denied``, then the auth store will be modified and future
     /// requests from the same client will be rejected without a prompt.
