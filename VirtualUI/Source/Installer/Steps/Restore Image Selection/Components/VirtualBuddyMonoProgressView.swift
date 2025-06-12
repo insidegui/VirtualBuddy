d7290d1134eae6f162740c3308ff407dfe6d22e3
Gui Rambo <insidegui@gmail.com>
2025-06-12 13:17:29 -0300

    Use CA for mono progress bar due to high CPU usage with SwiftUI

--- VirtualUI/Source/Installer/Steps/Restore Image Selection/Components/VirtualBuddyMonoProgressView.swift
+++ VirtualUI/Source/Installer/Steps/Restore Image Selection/Components/VirtualBuddyMonoProgressView.swift
@@ -53,13 +53,9 @@ private struct RamRodProgressView: View {
 
     var body: some View {
         ZStack {
-            Rectangle()
-                .fill(Color(white: 0.16))
-            GeometryReader { proxy in
-                Color(white: 0.82)
-                    .frame(width: proxy.size.width * (progress / 1.0), alignment: .leading)
-                    .animation(.linear, value: progress)
-            }
+            Rectangle().fill(Color(white: 0.16))
+
+            ProgressBarShapeView(progress: progress)
         }
             .clipShape(shape)
             .overlay(shape.stroke(Color(white: 0.25), lineWidth: 1))
@@ -68,6 +64,62 @@ private struct RamRodProgressView: View {
 
     private var shape: some InsettableShape {
         Capsule(style: .continuous)
+    }
+}
+
+/**
+ You see, animating a white rectangle growing in width is a very expensive operation that SwiftUI
+ is completely unable to do by itself without consuming an unhealthy amount of CPU,
+ so this uses Core Animation instead to offload that expensive computation to the WindowServer/GPU.
+ */
+private struct ProgressBarShapeView: NSViewRepresentable {
+    typealias NSViewType = _Representable
+
+    var progress: Double = 0
+
+    func makeNSView(context: Context) -> _Representable {
+        _Representable(frame: .zero)
+    }
+
+    func updateNSView(_ nsView: _Representable, context: Context) {
+        nsView.progress = progress
+    }
+
+    final class _Representable: NSView {
+        private lazy var bar = CALayer()
+
+        @Invalidating(.layout)
+        var progress: Double = 0
+
+        override init(frame frameRect: NSRect) {
+            super.init(frame: frameRect)
+
+            platformLayer.addSublayer(bar)
+            bar.backgroundColor = .white
+            bar.anchorPoint = CGPoint(x: 0, y: 0.5)
+        }
+
+        required init?(coder: NSCoder) {
+            fatalError()
+        }
+
+        override func layout() {
+            super.layout()
+
+            CATransaction.begin()
+            CATransaction.setDisableActions(true)
+
+            bar.position = CGPoint(x: bounds.minX, y: bounds.midY)
+            bar.frame.size.height = bounds.height
+
+            CATransaction.commit()
+
+            CATransaction.begin()
+            CATransaction.setAnimationDuration(0.2)
+            CATransaction.setAnimationTimingFunction(.init(name: .linear))
+            bar.frame.size.width = bounds.width * progress
+            CATransaction.commit()
+        }
     }
 }
 
