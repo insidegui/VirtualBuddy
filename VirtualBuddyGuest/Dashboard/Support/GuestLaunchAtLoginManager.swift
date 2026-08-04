48f78a92a45a2ebec0f7470f794832474de0353a
Guilherme Rambo <insidegui@gmail.com>
2026-08-04 09:15:09 -0700

    Register VirtualBuddyGuest itself as login item (#733)

--- VirtualBuddyGuest/Dashboard/Support/GuestLaunchAtLoginManager.swift
+++ VirtualBuddyGuest/Dashboard/Support/GuestLaunchAtLoginManager.swift
@@ -6,21 +6,21 @@ final class GuestLaunchAtLoginManager: ObservableObject {
 
     private lazy var logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: String(describing: Self.self))
 
-    var isLaunchAtLoginEnabled: Bool { SMAppService.guestHelper.status == .enabled }
+    var isLaunchAtLoginEnabled: Bool { SMAppService.mainApp.status == .enabled }
 
     func setLaunchAtLoginEnabled(_ enabled: Bool) async throws {
         logger.debug("Set launch at login enabled: \(enabled, privacy: .public)")
 
         if enabled {
-            try SMAppService.guestHelper.register()
+            try SMAppService.mainApp.register()
         } else {
-            try await SMAppService.guestHelper.unregister()
+            try await SMAppService.mainApp.unregister()
         }
 
         await MainActor.run { objectWillChange.send() }
     }
 
-    private var hasAutoEnabled: Bool {
+    private var hasAutoEnabledMainAppService: Bool {
         get { UserDefaults.standard.bool(forKey: #function) }
         set {
             UserDefaults.standard.set(newValue, forKey: #function)
@@ -29,16 +29,18 @@ final class GuestLaunchAtLoginManager: ObservableObject {
     }
 
     func autoEnableIfNeeded() {
-        guard !hasAutoEnabled else { return }
-        hasAutoEnabled = true
+        unregisterLegacyLoginItemIfNeeded()
 
-        logger.debug("Attempting to auto-enable launch at login")
+        guard !hasAutoEnabledMainAppService else { return }
+        hasAutoEnabledMainAppService = true
+
+        logger.notice("Attempting to auto-enable launch at login")
 
         Task {
             do {
                 try await setLaunchAtLoginEnabled(true)
 
-                logger.debug("Successfully auto-enabled launch at login")
+                logger.notice("Successfully auto-enabled launch at login")
             } catch {
                 logger.error("Failed to auto-enable launch at login: \(error, privacy: .public)")
             }
@@ -47,7 +49,19 @@ final class GuestLaunchAtLoginManager: ObservableObject {
 
 }
 
-@available(macOS 13.0, *)
+private extension GuestLaunchAtLoginManager {
+    func unregisterLegacyLoginItemIfNeeded() {
+        /// Try to unregister regardless of the helper status since it's possible for it to not report enabled but still show up in System Settings.
+        do {
+            try SMAppService.legacyGuestHelper.unregister()
+        } catch {
+            guard SMAppService.legacyGuestHelper.status == .enabled else { return }
+
+            logger.fault("Error unregistering legacy guest helper login item - \(error, privacy: .public)")
+        }
+    }
+}
+
 private extension SMAppService {
-    static let guestHelper = SMAppService.loginItem(identifier: kGuestAppLaunchAtLoginHelperBundleID)
+    static let legacyGuestHelper = SMAppService.loginItem(identifier: kLegacyGuestAppLaunchAtLoginHelperBundleID)
 }
