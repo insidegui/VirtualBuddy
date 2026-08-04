50c0a9fb3d1910a4d36de036f4edddd6dcb630f6
Guilherme Rambo <insidegui@gmail.com>
2026-08-04 13:14:33 -0300

    Register VirtualBuddyGuest itself as login item

--- VirtualBuddyGuest/Dashboard/Support/VirtualBuddyGuest-Bridging-Header.h
+++ VirtualBuddyGuest/Dashboard/Support/VirtualBuddyGuest-Bridging-Header.h
@@ -1,1 +1,3 @@
-#define kGuestAppLaunchAtLoginHelperBundleID GUEST_LAUNCH_AT_LOGIN_HELPER_BUNDLE_ID
+/// VirtualBuddyGuest previously used a separate launch at login helper, but since VirtualBuddy 2.2 beta 3, it registers itself as a login item.
+/// This identifier and the legacy helper target are kept so that VirtualBuddyGuest can unregister it.
+#define kLegacyGuestAppLaunchAtLoginHelperBundleID LEGACY_GUEST_LAUNCH_AT_LOGIN_HELPER_BUNDLE_ID
